8f5d9fe800387e7ef8ec617db8340ca548df28ce
TuuliinaOkkolin <tuuliinaokk@gmail.com>
2026-05-04 12:27:02 +0300

    Lisätty playeriin liikkeet

--- gamejam/Code/Characters/player.gd
+++ gamejam/Code/Characters/player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
+class_name Player
 
+const SPEED = 150.0
+const JUMP_VELOCITY = -250.0
+ 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,12 +14,19 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	# Get the input direction: -1, 0, 1
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	# Flip the Sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# Apply movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
