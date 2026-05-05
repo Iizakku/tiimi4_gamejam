0f0be0320ff127a2fa590ddaaddcad553c47138d
Pumbaah1 <niklashummel98@gmail.com>
2026-05-05 09:47:56 +0300

    Vihollis spawnerien lisäys

---
+++ gamejam/Code/Level/spawner.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+@export var enemy_scene : PackedScene
+
+
+func _on_timer_timeout() -> void:
+	var enemy = enemy_scene.instantiate()
+	enemy.position = position
+	get_parent().add_child(enemy)
