f6ca767a1fd3fa9639038366983a8df2ce1a3e55
juuliamk <juulia99.kuusikko@gmail.com>
2026-05-06 14:26:43 +0300

    Pause menu ja Main menu muokkaukset

---
+++ gamejam/pause_menu.gd
@@ -0,0 +1,25 @@
+extends Control
+
+var _is_paused: bool = false
+
+func _ready() -> void:
+	set_paused(false) # piilottaa menun alussa
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		set_paused(!_is_paused)
+
+func set_paused(value: bool) -> void:
+	_is_paused = value
+	get_tree().paused = value
+	visible = value
+
+func _on_resume_pressed() -> void:
+	set_paused(false)
+
+func _on_main_menu_pressed() -> void:
+	get_tree().paused = false # tärkeä!
+	get_tree().change_scene_to_file("res://Scenes/Levels/MainMenu.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
