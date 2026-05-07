37ca189d22974b98cf16b85281e518b97474325a
TuuliinaOkkolin <tuuliinaokk@gmail.com>
2026-05-07 16:38:35 +0300

    lisäkorjauksia koodiin

--- gamejam/Code/UI/scorelabel.gd
+++ gamejam/Code/UI/scorelabel.gd
@@ -6,8 +6,6 @@ func _ready() -> void:
 	
 	update_label(GameManager.get_score())
 	GameManager.score_changed.connect(update_label)
-	update_label(GameManager.get_score())
-	GameManager.score_changed.connect(update_label)
 	
 func update_label(new_score: int) -> void:
 	text = "SCORE: " + str(new_score)
